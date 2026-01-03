97007867b22567481fc91c08e33853cebae1482b
Shane Grigsby <refuge@rocktalus.com>
2026-01-02 17:02:41 -0800

    removing test for climkitae

--- variables.tf
+++ variables.tf
@@ -520,9 +520,13 @@ variable "lifecycle_hooks_enabled" {
 }
 
 variable "lifecycle_post_start_command" {
-  description = "Command to run after container starts (shell command as list)"
+  description = "Command to run after container starts (installs latest climakitae and clones notebooks)"
   type        = list(string)
-  default     = ["sh", "-c", "python -m nbgitpuller https://github.com/cal-adapt/cae-notebooks main cae-notebooks || true"]
+  default = [
+    "sh",
+    "-c",
+    "/srv/conda/envs/notebook/bin/pip install --no-deps -e git+https://github.com/cal-adapt/climakitae.git#egg=climakitae -e git+https://github.com/cal-adapt/climakitaegui.git#egg=climakitaegui; /srv/conda/envs/notebook/bin/gitpuller https://github.com/cal-adapt/cae-notebooks main cae-notebooks || true"
+  ]
 }
 
 # Existing S3 Bucket Configuration
